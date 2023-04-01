91ac5adacef393cce87c3e174770fca9b3d91966
emmajane1313 <admin@digitalax.xyz>
2023-04-01 08:08:15 -0400

    deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,30 +1,36 @@
-import { ethers, network, run } from "hardhat";
+import { ethers, run } from "hardhat";
 
 const main = async () => {
   try {
-    // const Parent = await ethers.getContractFactory("ParentTemplates");
-    // const Child = await ethers.getContractFactory("ChildTemplates");
+    const AccessControl = await ethers.getContractFactory("AccessControl");
+    const TheDialWhitelist = await ethers.getContractFactory(
+      "TheDialWhitelist"
+    );
 
-    // const child = await Child.deploy("ChildTemplates", "CTFGO");
-    // const parent = await Parent.deploy(child.address);
+    const accessControl = await AccessControl.deploy("AccessControl", "ACON");
+    const theDialWhitelist = await TheDialWhitelist.deploy(
+      accessControl.address
+    );
 
-    // const WAIT_BLOCK_CONFIRMATIONS = 20;
+    const WAIT_BLOCK_CONFIRMATIONS = 20;
 
-    // child.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
-    // parent.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    accessControl.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    theDialWhitelist.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
 
-    // console.log(`Parent Templates Contract deployed at\n${parent.address}`);
-    // console.log(`Child Templates Contract deployed at\n${child.address}`);
+    console.log(`Whitelist Contract deployed at\n${theDialWhitelist.address}`);
+    console.log(
+      `Access Control Contract deployed at\n${accessControl.address}`
+    );
 
-    await run(`verify:verify`, {
-      address: "0xC1D06Ea0AFEBADC943708239Aa7642fA93B955E0",
-      constructorArguments: ["ChildTemplates", "CTFGO"],
-    });
+    // await run(`verify:verify`, {
+    //   address: "",
+    //   constructorArguments: ["Access Control", "ACON"],
+    // });
 
-    await run(`verify:verify`, {
-      address: "0x6Eba8878953de820FCb98Ba795FC05a90f30e0b7",
-      constructorArguments: ["0xC1D06Ea0AFEBADC943708239Aa7642fA93B955E0"],
-    });
+    // await run(`verify:verify`, {
+    //   address: "",
+    //   constructorArguments: [""],
+    // });
   } catch (err: any) {
     console.error(err.message);
   }
