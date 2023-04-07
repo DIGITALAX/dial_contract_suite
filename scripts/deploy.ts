44aee9a86bc8e512349b5e943bdb1e3e83196808
emmajane1313 <admin@digitalax.xyz>
2023-04-07 06:24:26 -0400

    deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,7 +7,7 @@ const main = async () => {
       "TheDialWhitelist"
     );
 
-    const accessControl = await AccessControl.deploy("AccessControl", "ACON");
+    const accessControl = await AccessControl.deploy("AccessControl", "CHROA");
     const theDialWhitelist = await TheDialWhitelist.deploy(
       accessControl.address
     );
@@ -22,15 +22,132 @@ const main = async () => {
       `Access Control Contract deployed at\n${accessControl.address}`
     );
 
-    // await run(`verify:verify`, {
-    //   address: "",
-    //   constructorArguments: ["Access Control", "ACON"],
-    // });
+    const ChromadinEscrow = await ethers.getContractFactory("ChromadinEscrow");
+    const ChromadinCollection = await ethers.getContractFactory(
+      "ChromadinCollection"
+    );
+    const ChromadinNFT = await ethers.getContractFactory("ChromadinNFT");
+    const ChromadinPayment = await ethers.getContractFactory(
+      "ChromadinPayment"
+    );
+    const ChromadinMarketplace = await ethers.getContractFactory(
+      "ChromadinMarketplace"
+    );
+    const ChromadinDrop = await ethers.getContractFactory("ChromadinDrop");
 
-    // await run(`verify:verify`, {
-    //   address: "",
-    //   constructorArguments: [""],
-    // });
+    const chromadinPayment = await ChromadinPayment.deploy(
+      accessControl.address
+    );
+    const chromadinNFT = await ChromadinNFT.deploy(accessControl.address);
+    const chromadinCollection = await ChromadinCollection.deploy(
+      chromadinNFT.address,
+      accessControl.address,
+      chromadinPayment.address,
+      "Chromadin Collection",
+      "CHROC"
+    );
+    const chromadinMarketplace = await ChromadinMarketplace.deploy(
+      chromadinCollection.address,
+      accessControl.address,
+      chromadinNFT.address,
+      "Chromadin Marketplace",
+      "CHROM"
+    );
+    const chromadinDrop = await ChromadinDrop.deploy(
+      chromadinCollection.address,
+      accessControl.address,
+      "Chromadin Drop",
+      "CHROD"
+    );
+    const chromadinEscrow = await ChromadinEscrow.deploy(
+      chromadinCollection.address,
+      chromadinMarketplace.address,
+      accessControl.address,
+      chromadinNFT.address,
+      "Chromadin Escrow",
+      "CHROE"
+    );
+
+    chromadinPayment.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    chromadinNFT.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    chromadinCollection.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    chromadinMarketplace.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    chromadinDrop.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+    chromadinEscrow.deployTransaction.wait(WAIT_BLOCK_CONFIRMATIONS);
+
+    console.log(
+      `Chromadin Payment Contract deployed at\n${chromadinPayment.address}`
+    );
+    console.log(`Chromadin NFT Contract deployed at\n${chromadinNFT.address}`);
+    console.log(
+      `Chromadin Collection Contract deployed at\n${chromadinCollection.address}`
+    );
+    console.log(
+      `Chromadin Marketplace Contract deployed at\n${chromadinMarketplace.address}`
+    );
+    console.log(
+      `Chromadin Drop Contract deployed at\n${chromadinDrop.address}`
+    );
+    console.log(
+      `Chromadin Escrow Contract deployed at\n${chromadinEscrow.address}`
+    );
+
+    await run(`verify:verify`, {
+      address: accessControl.address,
+      constructorArguments: ["AccessControl", "CHROA"],
+    });
+    await run(`verify:verify`, {
+      address: theDialWhitelist.address,
+      constructorArguments: [accessControl.address],
+    });
+    await run(`verify:verify`, {
+      address: chromadinPayment.address,
+      constructorArguments: [accessControl.address],
+    });
+    await run(`verify:verify`, {
+      address: chromadinNFT.address,
+      constructorArguments: [accessControl.address],
+    });
+    await run(`verify:verify`, {
+      address: chromadinCollection.address,
+      constructorArguments: [
+        chromadinNFT.address,
+        accessControl.address,
+        chromadinPayment.address,
+        "Chromadin Collection",
+        "CHROC",
+      ],
+    });
+    await run(`verify:verify`, {
+      address: chromadinMarketplace.address,
+      constructorArguments: [
+        chromadinCollection.address,
+        accessControl.address,
+        chromadinNFT.address,
+        "Chromadin Marketplace",
+        "CHROM",
+      ],
+    });
+    await run(`verify:verify`, {
+      address: chromadinDrop.address,
+      constructorArguments: [
+        chromadinCollection.address,
+        accessControl.address,
+        "Chromadin Drop",
+        "CHROD",
+      ],
+    });
+    await run(`verify:verify`, {
+      address: chromadinEscrow.address,
+      constructorArguments: [
+        chromadinCollection.address,
+        chromadinMarketplace.address,
+        accessControl.address,
+        chromadinNFT.address,
+        "Chromadin Escrow",
+        "CHROE",
+      ],
+    });
   } catch (err: any) {
     console.error(err.message);
   }
